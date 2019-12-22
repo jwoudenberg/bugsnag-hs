3de73ba6aaf6b7b5316a4f750764204d7383daed
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-12-22 23:00:01 +0100

    Add module for buffering operations
    We have an API that we can use to write reports of multiple events, but
    we don't offer any help for batching up these events. We'd like to add
    an additional API for writing individual events, but in a way that they
    end up getting batched into HTTP requests.
    
    This commit sets up the `Buffer` abstraction that will allow us to get
    there.

---
+++ src/Data/Buffer.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+-- |
+-- A buffer for batched write operations. Push individual items into the buffer
+-- and provide an operation that writes out batches of them.
+module Data.Buffer
+  ( Buffer,
+    Settings (..),
+    new,
+    push,
+    flush,
+  )
+where
+
+import qualified Control.Concurrent.STM as STM
+import qualified Control.Debounce as Debounce
+import qualified Data.List.NonEmpty as NonEmpty
+import GHC.Natural (Natural)
+
+data Buffer a
+  = Buffer
+      { -- | Push a new item into the buffer.
+        push :: a -> IO (),
+        -- | Flush the current contents of the buffer.
+        flush :: IO ()
+      }
+
+data Settings a
+  = Settings
+      { -- | Function to write a batch of items.
+        write :: NonEmpty.NonEmpty a -> IO (),
+        -- | The maximum amount of items to write in a single batch.
+        size :: Natural,
+        -- | The frequency with which the buffer gets flushed automatically.
+        frequencyInMicroSeconds :: Int
+      }
+
+new :: Settings a -> IO (Buffer a)
+new settings = do
+  queue <- STM.atomically $ STM.newTBQueue (size settings)
+  let writeList xs = maybe (pure ()) (write settings) (NonEmpty.nonEmpty xs)
+  let flush = writeList =<< STM.atomically (STM.flushTBQueue queue)
+  scheduleFlush <-
+    Debounce.mkDebounce
+      Debounce.defaultDebounceSettings
+        { Debounce.debounceAction = flush,
+          Debounce.debounceFreq = frequencyInMicroSeconds settings,
+          Debounce.debounceEdge = Debounce.trailingEdge
+        }
+  let push x = do
+        overflow <- STM.atomically $ do
+          full <- STM.isFullTBQueue queue
+          toFlush <- if full then STM.flushTBQueue queue else pure []
+          STM.writeTBQueue queue x
+          pure toFlush
+        -- Flush the items that overflow the queue immediately.
+        writeList overflow
+        -- Schedule a regular flush for the rest.
+        scheduleFlush
+  pure Buffer {push, flush}
