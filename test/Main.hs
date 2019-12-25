126e503af9e8183d2496aab874a515ad1e85e419
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-12-25 08:53:25 +0100

    Move existing test into subdirectory
    This makes place for an additional test.

--- test/Main.hs
+++ test/Main.hs
@@ -1,25 +1,10 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Main
   ( main,
   )
 where
 
-import qualified Data.ByteString
-import qualified Data.Aeson
-import qualified Network.Bugsnag as Bugsnag
-import qualified System.Exit
+import qualified Spec.Network.Bugsnag
 
--- |
--- We don't have a schema to validate our generated JSON against, but we do
--- have a sample report document we can pull from the API website. If we parse
--- that using decoders configured in the same way our encoders are, then we have
--- some guarantee (although not a complete one) that our encoders produce the
--- correct format.
 main :: IO ()
 main = do
-  json <- Data.ByteString.readFile "./test/sample-report.json"
-  let result = Data.Aeson.eitherDecodeStrict' json
-  case result of
-    Right (_ :: Bugsnag.Report) -> pure ()
-    Left err -> System.Exit.die err
+  Spec.Network.Bugsnag.tests
---
+++ test/Spec/Network/Bugsnag.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Spec.Network.Bugsnag
+  ( tests,
+  )
+where
+
+import qualified Data.Aeson
+import qualified Data.ByteString
+import qualified Network.Bugsnag as Bugsnag
+import qualified System.Exit
+
+-- |
+-- We don't have a schema to validate our generated JSON against, but we do
+-- have a sample report document we can pull from the API website. If we parse
+-- that using decoders configured in the same way our encoders are, then we have
+-- some guarantee (although not a complete one) that our encoders produce the
+-- correct format.
+tests :: IO ()
+tests = do
+  json <- Data.ByteString.readFile "./test/sample-report.json"
+  let result = Data.Aeson.eitherDecodeStrict' json
+  case result of
+    Right (_ :: Bugsnag.Report) -> pure ()
+    Left err -> System.Exit.die err
