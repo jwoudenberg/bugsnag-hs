ee4555f4d816f381069cd049a6d7d0d8f3f01330
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-12-25 11:28:05 +0100

    Add test for `Data.Buffer` code

--- test/Main.hs
+++ test/Main.hs
@@ -3,8 +3,10 @@ module Main
   )
 where
 
+import qualified Spec.Data.Buffer
 import qualified Spec.Network.Bugsnag
 
 main :: IO ()
 main = do
   Spec.Network.Bugsnag.tests
+  Spec.Data.Buffer.tests
