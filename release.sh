1c265b6efaa11cac129649b77afcbaed63313e53
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-06-14 18:29:30 +0200

    Commmit cabal file
    This is the new recommendation from the stack team.

--- release.sh
+++ release.sh
@@ -6,6 +6,12 @@ name=$(grep name: < package.yaml | awk '{print $2}')
 version=$(grep version: < package.yaml | awk '{print $2}')
 bundle="$name-$version.tar.gz"
 
+hpack
+
+if git status --porcelain | grep . ; then
+  fail "Stash any changes before starting the release script."
+fi
+
 # check changelog contains an entry for this version
 grep "^# $version$" < CHANGELOG.md
 
@@ -17,7 +23,6 @@ grep "Copyright (c) $(date +'%Y')" < LICENSE
 git fetch --tags
 git tag -l --points-at HEAD | grep "^$version$"
 
-hpack
 cabal sdist -o - > "$bundle"
 cabal upload --publish "$bundle"
 cabal upload -d --publish
